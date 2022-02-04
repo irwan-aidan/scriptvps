ac43760e9c8233088218da1b0374719e948c37c8
irwan-aidan <91520148+irwan-aidan@users.noreply.github.com>
2022-02-04 20:15:35 +0800

    Add files via upload

---
+++ ssh/addhost.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# My Telegram : https://t.me/Akbar218
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+clear
+read -rp "Domain/Host : " -e domain
+echo "IP=$domain" >>/var/lib/vpn/ipvps.conf
+echo $domain > /etc/xray/domain
+echo start
+systemctl stop xray.service
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key --ecc
+systemctl start xray.service
+echo Done
+sleep 1.5
+clear
+neofetch
