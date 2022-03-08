9f759ea81dab3d3dcabe3f34f076ea8ab08334ec
mohd irwan mohi <91520148+irwan-aidan@users.noreply.github.com>
2022-03-09 02:35:55 +0800

    Add files via upload

---
+++ menu/README.md
@@ -0,0 +1,2 @@
+# menu
+git clone https://github.com/nandunkz/menu.git && cd menu && chmod +x install.sh && ./install.sh && cd && rm -rf menu
---
+++ menu/install.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+clear
+rm /usr/local/sbin/accounts
+rm /usr/local/sbin/base-ports
+rm /usr/local/sbin/base-ports-wc
+rm /usr/local/sbin/base-script
+rm /usr/local/sbin/clearcache
+rm /usr/local/sbin/connections
+rm /usr/local/sbin/create
+rm /usr/local/sbin/create_random
+rm /usr/local/sbin/create_trial
+rm /usr/local/sbin/delete_expired
+rm /usr/local/sbin/diagnose
+rm /usr/local/sbin/edit_dropbear
+rm /usr/local/sbin/edit_openssh
+rm /usr/local/sbin/edit_openvpn
+rm /usr/local/sbin/edit_ports
+rm /usr/local/sbin/edit_squid3
+rm /usr/local/sbin/edit_stunnel4
+rm /usr/local/sbin/locked_list
+rm /usr/local/sbin/menu
+rm /usr/local/sbin/options
+rm /usr/local/sbin/reboot_sys
+rm /usr/local/sbin/reboot_sys_auto
+rm /usr/local/sbin/restart_services
+rm /usr/local/sbin/server
+rm /usr/local/sbin/set_multilogin_autokill
+rm /usr/local/sbin/set_multilogin_autokill_lib
+rm /usr/local/sbin/show_ports
+rm /usr/local/sbin/user_delete
+rm /usr/local/sbin/user_details
+rm /usr/local/sbin/user_details_lib
+rm /usr/local/sbin/user_extend
+rm /usr/local/sbin/user_list
+rm /usr/local/sbin/user_lock
+rm /usr/local/sbin/user_unlock
+cd
+cd menu
+chmod +x *
+mv accounts /usr/local/sbin/accounts
+mv base-ports /usr/local/sbin/base-ports
+mv base-ports-wc /usr/local/sbin/base-ports-wc
+mv base-script /usr/local/sbin/base-script
+mv clearcache /usr/local/sbin/clearcache
+mv connections /usr/local/sbin/connections
+mv create /usr/local/sbin/create
+mv create_random /usr/local/sbin/create_random
+mv create_trial /usr/local/sbin/create_trial
+mv delete_expired /usr/local/sbin/delete_expired
+mv diagnose /usr/local/sbin/diagnose
+mv edit_dropbear /usr/local/sbin/edit_dropbear
+mv edit_openssh /usr/local/sbin/edit_openssh
+mv edit_openvpn /usr/local/sbin/edit_openvpn
+mv edit_ports /usr/local/sbin/edit_ports
+mv edit_squid3 /usr/local/sbin/edit_squid3
+mv edit_stunnel4 /usr/local/sbin/edit_stunnel4
+mv locked_list /usr/local/sbin/locked_list
+mv menu /usr/local/sbin/menu
+mv options /usr/local/sbin/options
+mv reboot_sys /usr/local/sbin/reboot_sys
+mv reboot_sys_auto /usr/local/sbin/reboot_sys_auto
+mv restart_services /usr/local/sbin/restart_services
+mv server /usr/local/sbin/server
+mv set_multilogin_autokill /usr/local/sbin/set_multilogin_autokill
+mv set_multilogin_autokill_lib /usr/local/sbin/set_multilogin_autokill_lib
+mv show_ports /usr/local/sbin/show_ports
+mv user_delete /usr/local/sbin/user_delete
+mv user_details /usr/local/sbin/user_details
+mv user_details_lib /usr/local/sbin/user_details_lib
+mv user_extend /usr/local/sbin/user_extend
+mv user_list /usr/local/sbin/user_list
+mv user_lock /usr/local/sbin/user_lock
+mv user_unlock /usr/local/sbin/user_unlock
+cd /usr/local/sbin
+chmod +x accounts
+
+chmod +x base-ports
+
+chmod +x base-ports-wc
+
+chmod +x base-script
+
+chmod +x clearcache
+
+chmod +x connections
+
+chmod +x create
+
+chmod +x create_random
+
+chmod +x create_trial
+
+chmod +x delete_expired
+
+chmod +x diagnose
+
+chmod +x edit_dropbear
+
+chmod +x edit_openssh
+
+chmod +x edit_openvpn
+
+chmod +x edit_ports
+
+chmod +x edit_squid3
+
+chmod +x edit_stunnel4
+
+chmod +x locked_list
+
+chmod +x menu
+
+chmod +x options
+
+chmod +x reboot_sys
+
+chmod +x reboot_sys_auto
+
+chmod +x restart_services
+
+chmod +x server
+
+chmod +x set_multilogin_autokill
+
+chmod +x set_multilogin_autokill_lib
+
+chmod +x show_ports
+
+chmod +x user_delete
+
+chmod +x user_details
+
+chmod +x user_details_lib
+
+chmod +x user_extend
+
+chmod +x user_list
+
+chmod +x user_lock
+
+chmod +x user_unlock
+
+cd
+apt install figlet
+
+exit
